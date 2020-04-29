96af91c7833a56f8679bc263cf943d0d53c9a6d8
realakshay <gaikwadakshay663@gmail.com>
2020-04-29 10:30:38 +0530

    Modify : Here complete the functions for student result model

---
+++ models/result.py
@@ -0,0 +1,30 @@
+from db import db
+
+class ResultModel(db.Model):
+
+    __tablename__="results"
+
+    id=db.Column(db.Integer)
+    prn=db.Column(db.Integer, primary_key=True)
+    cgpa=db.Column(db.Float, nullable=False)
+
+    students=db.relationship('StudentModel', lazy="dynamic")
+
+    def __init__(self,prn,cgpa):
+        self.prn=prn
+        self.cgpa=cgpa
+
+    def json(self):
+        return {"prn":self.prn, "cgpa":self.cgpa, "student":[x.json() for x in self.students.all()]}
+    
+    @classmethod
+    def find_by_prn(cls,prn):
+        return cls.query.filter_by(prn=prn).first()
+    
+    def insert_in_db(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete_from_db(self):
+        db.session.add(self)
+        db.session.commit()
