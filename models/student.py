541a85121db351185db0e63b137b7bb246a79454
realakshay <gaikwadakshay663@gmail.com>
2020-04-29 09:20:32 +0530

    Modify : Additional field add department

--- models/student.py
+++ models/student.py
@@ -4,23 +4,26 @@ class StudentModel(db.Model):
 
     __tablename__="students"
 
+    id=db.Column(db.Integer)
     prn=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(50), nullable=False)
     mobile=db.Column(db.String(10), unique=True, nullable=False)
     email=db.Column(db.String(150), unique=True, nullable=False)
+    department=db.Column(db.String(80),nullable=False)
     classid=db.Column(db.Integer, db.ForeignKey('classes.id'))
 
     classes=db.relationship('ClassModel')
 
-    def __init__(self, prn, name, mobile, email, classid):
+    def __init__(self, prn, name, mobile, email, department, classid):
         self.prn=prn
         self.name=name
         self.mobile=mobile
         self.email=email
+        self.department=department
         self.classid=classid
 
     def json(self):
-        return {"prn":self.prn, "name":self.name, "mobile":self.mobile, "email":self.email, "classId":self.classid}
+        return {"prn":self.prn, "name":self.name, "mobile":self.mobile, "email":self.email, "department":self.department, "classId":self.classid}
     
     @classmethod
     def find_by_prn(cls,prn):
@@ -37,7 +40,11 @@ class StudentModel(db.Model):
     def delete_from_db(self):
         db.session.delete(self)
         db.session.commit()
-        
+    
+    @classmethod
+    def find_by_department(cls, department):
+        return cls.query.filter_by(department=department)
+
     @classmethod
     def find_all(cls):
         return cls.query.all()
