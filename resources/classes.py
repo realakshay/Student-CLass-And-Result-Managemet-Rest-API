de529edec439ee9d96842a9ceaeba1e803fb742e
realakshay <gaikwadakshay663@gmail.com>
2020-04-29 19:55:01 +0530

    Modify : Here resource written start for class module. first get request write

---
+++ resources/classes.py
@@ -0,0 +1,23 @@
+from flask_restful import Resource, reqparse
+from models.classes import ClassModel
+
+class ClassReource(Resource):
+    
+    parser=reqparse.RequestParser()
+    parser.add_argument("classname", type=str, required=True, help="cannot be blank")
+    parser.add_argument("cc", type=str, required=True, help="cannot be blank")
+
+    def get(self,classname):
+        classes=ClassModel.find_by_classname(classname)
+        if classes:
+            return classes.json()
+        return {"message":"Not found"}
+    
+    def post(self,classname):
+        data=ClassReource.parser.parse_args()
+        classes=ClassModel.find_by_classname(classname)
+        if classes:
+            return {"message":"This class already exist"}
+        classes=ClassModel(data['classname'], data['cc'])
+        classes.insert_in_db()
+        return {"message":"Insert Success"}
