b5224148b23cca8818e07ac54b516c8445bdeca3
realakshay <gaikwadakshay663@gmail.com>
2020-05-02 21:04:24 +0530

    Modify : Here code for optional jwt required for get the complete student details

--- resources/student.py
+++ resources/student.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.student import StudentModel
 
 class StudentResource(Resource):
@@ -55,11 +55,16 @@ class StudentResource(Resource):
 
 class StudentListResource(Resource):
 
-    @jwt_required
+    @jwt_optional
     def get(self):
+        user_id=get_jwt_identity()
+        print(user_id)
         result=StudentModel.get_all()
         students=[]
         for i in result:
             l={"PRN":i[0],"Name":i[1], "Mobile":i[2], "Email":i[3], "Department":i[4],"Class":i[5], "Class Co.":i[6], "cgpa":i[7]}
             students.append(l)
-        return {"Students":students}
+
+        if user_id:
+            return {"Students":students},200
+        return {"Students":[x["Name"] for x in students], "Message":"Login to more info"},200
